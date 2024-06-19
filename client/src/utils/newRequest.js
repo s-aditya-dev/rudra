6f01847c734d587664bd13efd5296c07b92e3a54
Aditya Pratap Singh <138649643+s-aditya-dev@users.noreply.github.com>
2024-06-19 14:41:27 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://rudra-backend-4hzl.onrender.com/api/",
   withCredentials: true,
 });
 
