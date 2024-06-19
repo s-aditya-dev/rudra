d4e3292c171f2f7b5f317245c934a0f0f0d8ff6b
Aditya Pratap Singh <138649643+s-aditya-dev@users.noreply.github.com>
2024-06-19 14:42:48 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      "/user":"http://localhost:8000/api"
+      "/user":"https://rudra-backend-4hzl.onrender.com/api"
     }
   },
   plugins: [react()],
